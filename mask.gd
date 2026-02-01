ee3a18475b4098588ebf221a36a5beec373a7a50
Ben Yu <btyu@usc.edu>
2026-02-01 07:19:40 -0800

    ui placeholders

--- mask.gd
+++ mask.gd
@@ -1,5 +1,7 @@
 extends Area2D
 
+var collected:bool = false
+@export var mask:String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -8,4 +10,13 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if collected:
+		$Sprite2D.self_modulate.a -= .02
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if mask == "wing": $"../CharacterBody2D".wing_mask = true
+	elif mask == "heavy": $"../CharacterBody2D".heavy_mask = true
+	elif mask == "dash": $"../CharacterBody2D".dash_mask = true
+	#visible = false
+	collected = true
